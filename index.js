0efa71669349b4ef2d93c3e66604d2ee134d4a9d
Martin Rodriguez <54999192+ChasingFearTv@users.noreply.github.com>
2023-03-03 15:00:07 -0800

    touch or click
    added functionality.

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+const keysbutt = document.querySelectorAll('.key');
+
+keysbutt.forEach(key => {
+    key.addEventListener('pointerdown', e => {
+        // play sound
+        const audio = document.querySelector(`audio[data-key="${key.dataset.key}"]`);
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+
+        // add dot
+        const dot = document.createElement('div')
+        dot.classList.add('dot')
+        dot.id = e.pointerId
+        positionDot(e, dot)
+        document.body.append(dot)
+    })
+
+    key.addEventListener('pointermove', e => {
+        const dot = document.getElementById(e.pointerId)
+        if (dot == null) return
+        positionDot(e, dot)
+    })
+    key.addEventListener('pointerup', e => {
+        const dot = document.getElementById(e.pointerId)
+        if (dot == null) return
+        dot.remove()
+    })
+    key.addEventListener('pointercancel', e => {
+        const dot = document.getElementById(e.pointerId)
+        if (dot == null) return
+        dot.remove()
+    })
+})
+
+
+function positionDot(e, dot) {
+    dot.style.width = `${e.width * 2}px`
+    dot.style.height = `${e.height * 2}px`
+    dot.style.left = `${e.pageX}px`
+    dot.style.top = `${e.pageY}px`
+}
+
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -16,4 +59,4 @@ function removeTransition(e) {
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-window.addEventListener('keydown',playSound);
+window.addEventListener('keydown', playSound);
